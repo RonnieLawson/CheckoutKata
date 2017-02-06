d126acca54b4432797977b34e92611bd944f56d9
RonnieLawson <Ronnie.Lawson@esendex.com>
2017-02-06 08:46:32 +0000

    first commit

---
+++ CheckoutTests/CheckoutTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace CheckoutKata.Tests
+{
+    [TestFixture]
+    public class GivenACheckoutWithValidPricingRules
+    {
+        [Test]
+        public void ThenTheCheckoutIsCreated()
+        {
+            var checkout = new Checkout();
+            Assert.That(checkout.GetType(), Is.EqualTo(typeof(Checkout)));
+        }
+
+        [Test]
+        public void ThenThePriceingRulesAreStored()
+        {
+            var checkout = new Checkout();
+            Assert.That(checkout.GetType(), Is.EqualTo(typeof(Checkout)));
+        }
+    }
+}
